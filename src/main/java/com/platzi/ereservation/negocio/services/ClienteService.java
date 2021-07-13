37b12a76ad0bfaed576f01ece873c7e26163f776
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-13 17:40:45 -0500

    Transactionality of a service

--- src/main/java/com/platzi/ereservation/negocio/services/ClienteService.java
+++ src/main/java/com/platzi/ereservation/negocio/services/ClienteService.java
@@ -4,56 +4,66 @@
 package com.platzi.ereservation.negocio.services;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.platzi.ereservation.modelo.Cliente;
 import com.platzi.ereservation.negocio.repository.ClienteRepository;
 
 /**
  * Clase para definir los servicios de Cliente
+ * 
  * @author lourdes.b.rodriguez
  *
  */
 @Service
+@Transactional(readOnly = true)
 public class ClienteService {
-	
+
 	private final ClienteRepository clienteRepository;
-	
+
 	public ClienteService(ClienteRepository clienteRepository) {
 		// TODO Auto-generated constructor stub
 		this.clienteRepository = clienteRepository;
 	}
-	
+
 	/**
 	 * Método para realizar la operación de guardar un cliente
+	 * 
 	 * @param cliente
 	 * @return
-	 * */
+	 */
+	@Transactional
 	public Cliente create(Cliente cliente) {
 		return this.clienteRepository.save(cliente);
 	}
-	
+
 	/**
 	 * Método para realizar la operación de actualizar un cliente
+	 * 
 	 * @param cliente
 	 * @return
-	 * */
+	 */
+	@Transactional
 	public Cliente update(Cliente cliente) {
 		return this.clienteRepository.save(cliente);
 	}
-	
+
 	/**
 	 * Método para realizar la operación de eliminar un cliente
+	 * 
 	 * @param cliente
-	 * */
+	 */
+	@Transactional
 	public void delete(Cliente cliente) {
-		return this.clienteRepository.delete(cliente);
+		this.clienteRepository.delete(cliente);
 	}
-	
+
 	/**
 	 * Método para consultar un cliente por su identificacion
+	 * 
 	 * @param identificacionCli
 	 * @return
-	 * */
+	 */
 	public Cliente findByIdentificacion(String identificacionCli) {
 		return this.clienteRepository.findByIdentificacion(identificacionCli);
 	}
