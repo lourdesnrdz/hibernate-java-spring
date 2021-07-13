22f0660198fecb3b12a8ee90d0f048ad43fde9af
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-13 13:15:41 -0500

    POJOS with lombok

---
+++ src/main/java/com/platzi/ereservation/modelo/Cliente.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package com.platzi.ereservation.modelo;
+
+import lombok.Data;
+
+/**
+ * Clase que representa la tabla Cliente
+ * @author lourdes.b.rodriguez
+ *
+ */
+@Data
+public class Cliente {
+	
+	String nombreCli;
+	String apellidoCli;
+	
+	public Cliente() {
+		
+	}
+
+}
