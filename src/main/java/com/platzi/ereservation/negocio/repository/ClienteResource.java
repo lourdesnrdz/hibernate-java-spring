b86045a4bad2f426412b802302e089401d60e307
lourdesnrdz <lourdesnrdz@gmail.com>
2021-07-14 10:07:32 -0500

    Web Services and Spring Rest

---
+++ src/main/java/com/platzi/ereservation/negocio/repository/ClienteResource.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.platzi.ereservation.negocio.repository;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Clase que representa el servicio web de cliente
+ * @author lourdes.b.rodriguez
+ *
+ */
+@RestController
+@RequestMapping("/api/cliente")
+public class ClienteResource {
+
+}
